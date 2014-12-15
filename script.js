43b9991268008ab4a13d54a7c9df6fea26499785
bryantt23 <bryantt23@users.noreply.github.com>
2014-12-14 23:53:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-$('p').html("jQuery magic in action!");
+  
+  var $grid = $('#pixelgrid');
+
+for (i = 0; i < 16; i++) 
+{
+    var row = '<div>';
+
+    for (j = 0; j < 16; j++)
+        row += '<div class="square">' + j + '</div>';
+
+    row += '</div>';
+
+    $grid.append(row);
+}
 
 });
